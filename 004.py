75d7590e07873561b7f3055078375c3166ad0da2
ftang <tfxidian@gmail.com>
2023-10-26 23:27:31 +0800

    使用bcc python 得到open相关的信息，比如谁打开了什么文件
    实现了打开了bpf_trace_printk: File opened by PID 7560 (VM Thread) 但是似乎没有输出文件名，only输出了进程信息

---
+++ 004.py
@@ -0,0 +1,29 @@
+from bcc import BPF
+
+# 编写eBPF程序
+program = """
+#include <uapi/linux/ptrace.h>
+#include <linux/sched.h>
+
+int trace_open(struct pt_regs *ctx)
+{
+    u32 pid = bpf_get_current_pid_tgid();
+    char comm[TASK_COMM_LEN];
+    bpf_get_current_comm(&comm, sizeof(comm));
+    char filename[256];
+    bpf_probe_read_user_str(&filename, sizeof(filename), (void *)PT_REGS_PARM1(ctx));
+    bpf_trace_printk("File opened by PID %d (%s): %s\\n", pid, comm, filename);
+    return 0;
+}
+"""
+
+# 创建BPF对象并加载程序
+b = BPF(text=program)
+b.attach_kprobe(event="do_sys_open", fn_name="trace_open")
+# b.attach_kprobe(event="do_filp_open", fn_name="trace_open")
+# b.attach_kprobe(event="do_sys_openat2", fn_name="trace_open")
+# b.attach_kprobe(event="do_sys_openat", fn_name="trace_open")
+# b.attach_kprobe(event="do_sys_open_execat", fn_name="trace_open")
+
+# 打印结果
+b.trace_print()
